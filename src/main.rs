22af348373e2ecdbd3825f22172040ef308d5eb1
Midnight Veil <midnightveil@fea.st>
2022-11-09 21:19:10 +1100

    Change permission to 770
    I don't know why 640 doesn't work lol

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ fn main() {
     let path = Path::new(&path_str);
 
     let sftp = sess.sftp().unwrap();
-    sftp.mkdir(path, 0o640).unwrap();
+    sftp.mkdir(path, 0o770).unwrap();
     for file in config.files {
         let mut v = Vec::new();
         File::open(&local_files_folder.join(file))
